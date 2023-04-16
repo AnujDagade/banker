c7edeb01125e016274dc59a389bb22b72305526a
Anuj <dagadeanuj@gmail.com>
2023-04-16 15:36:34 +0530

    Update linked.c

--- linked.c
+++ linked.c
@@ -1,48 +1,97 @@
+//
+// Created by gitpod on 4/8/23.
+//
 #include "stdio.h"
-#define MAX 10
-#define SIZE 3
-
+#include "string.h"
+#define MAX 30
 typedef struct bk
 {
     int alloc;
     struct bk *next;
 }Block;
 
-int main()
+typedef struct dr
 {
-  Block bit_vec[MAX];
+    char name[20];
+    int length;
+    int start;
+}DIR;
 
-    for (int i = 0; i < MAX; ++i) {
+DIR dir[20];
 
-        if(i==0 || i==1 || i==3)
-            bit_vec[i].alloc = 0;
-        else
-            bit_vec[i].alloc = 1;
-    }
+Block  bit_vec[MAX];
 
-    for (int i = 0; i < MAX; ++i) {
-        printf("\t%d", bit_vec[i].alloc);
-    }
-
+int create_file(int size, int vec_size)
+{
     int count =0;
-    Block *prev[MAX];
-    for (int i = 0; i < MAX; ++i) {
+    int is_first = 1;
+    int start = -1;
+    Block *prev[vec_size];
+    for (int i = 0; i < vec_size; ++i) {
 
-        if(bit_vec[i].alloc == 1 && count < SIZE)
+        if(bit_vec[i].alloc == 1 && count < size)
         {
+
             prev[count] = &bit_vec[i];
             prev[count]->alloc = 0;
+            if (is_first)
+            {
+                start = i;
+                is_first = 0;
+            }
             count++;
         }
     }
+    //Forming a chain
     for(int i=0; i<count-1; i++)
     {
         prev[i]->next = prev[i+1];
     }
 
-    printf("\n");
-    for (int i = 0; i < MAX; ++i) {
-        printf("\t%d", bit_vec[i].alloc);
+    return start;
+}
+
+int main()
+{
+    int ch=99, vec_size, file_size, count=0;
+    char fname[20];
+    printf("\nEnter vec size: ");
+    scanf("%d",&vec_size);
+    //init
+    for (int i = 0; i < vec_size; ++i) {
+        bit_vec[i].alloc = 1;
     }
 
+
+    do {
+        printf("\n1:Show bit vec\n2:Create new file\n3:Show Directory\n4:Delete file\n5:Exit\nChoice: ");
+        scanf("%d",&ch);
+        switch (ch) {
+            case 1:
+                printf("\nBit Vec\n");
+                for (int i = 0; i < vec_size; ++i) {
+                    printf("\t%d",bit_vec[i].alloc);
+                }
+                break;
+
+            case 2:
+                printf("\nEnter file name: ");
+                scanf("%s",fname);
+                strcpy(dir[count].name, fname);
+                printf("\nEnter file size: ");
+                scanf("%d",&file_size);
+                dir[count].length = file_size;
+                dir[count++].start = create_file(file_size, vec_size);
+                break;
+            case 3:
+                for(int i=0; i<count; i++)
+                {
+                    printf("\nName: %s", dir[i].name);
+                    printf("\nStart: %d",dir[i].start);
+                    printf("\nFile size: %d", dir[i].length);
+                }
+                break;
+        }
+
+    } while (ch != 5);
 }
